605b1444e1226d697dbff6fcbf613b033050e1ad
Pedro Shoji <33377462+Raygagz@users.noreply.github.com>
2019-08-22 10:59:07 -0300

    processo único

--- short_or_long_signal_identifier/short_or_long_signal_identifier.vhd
+++ short_or_long_signal_identifier/short_or_long_signal_identifier.vhd
@@ -19,7 +19,8 @@ use work.code_types.all;
 entity short_or_long_signal_identifier is
 	port (clock	: in std_logic;
 			button_input	: in std_logic;
-
+			--clear, backspace	: in std_logic;
+			
 			code_test0, code_test1, code_test2, code_test3, code_test4 : out integer range 0 to 2;
 			index_test	: out integer range 0 to MAX_CODE_LENGTH-1;
 			code_current_index_test	:	out integer range 0 to 2);
@@ -27,26 +28,28 @@ end short_or_long_signal_identifier;
 
 architecture verifier of short_or_long_signal_identifier is
 	-- Define o tempo em que o sinal mudara de curto para longo
-	constant short_long_border : integer := 25000000;		-- 0.5s
+	constant short_long_border : integer := 25;		-- 0.5s
+	
 	signal	code	: integer_vector	:= (others => 0);		-- Guarda a "letra" salva atualmente (0: desligado, 1: ponto, 2: traco)
-	signal current_index	: integer range 0 to MAX_CODE_LENGTH := 1;
+	signal current_index	: integer range 0 to MAX_CODE_LENGTH := 0;
 	signal next_value	: integer range 0 to 2 := 0;
 begin
 	process(clock, button_input)
 		variable count	: integer := 0;
 		variable index_variable	: integer range 0 to MAX_CODE_LENGTH := current_index;
-		--variable code_variable	: integer_vector := code;
 		variable new_value	: integer range 0 to 2 := next_value;
 	begin
 		if (clock'event and clock = '0') then
-			-- Botao ativo em baixo
-			if	(button_input = '1') then
-				if (new_value /= 0) then
+			
+			if	(button_input = '1') then	-- Se o botao esta solto
+
+				if (new_value /= 0) then	-- Se estava apertado antes soma indice
 					index_variable := index_variable + 1;
 					if (index_variable = MAX_CODE_LENGTH) then
 						index_variable := 0;
 					end if;
 				end if;
+				
 				next_value <= new_value;
 				new_value := 0;
 				count := 0;
@@ -60,6 +63,14 @@ begin
 				end if;
 				next_value <= new_value;
 			end if;
+			
+			for i in 0 to MAX_CODE_LENGTH-1 loop
+				case current_index is
+					when i => code(i) <= next_value;
+					when others => code(i) <= unaffected;
+				end case;
+			end loop;
+			
 		end if;
 		
 		current_index <= index_variable;
@@ -68,29 +79,10 @@ begin
 	
 	index_test <= current_index;
 	code_current_index_test <= next_value;
-
-	
-	--code(0) <= next_value when current_index = 0 else unaffected;
-	--code(1) <= next_value when current_index = 1 else unaffected;
-	--code(2) <= next_value when current_index = 2 else unaffected;
-	--code(3) <= next_value when current_index = 3 else unaffected;
-	--code(4) <= next_value when current_index = 4 else unaffected;
 	
 	--code_assigner:	for i in 0 to MAX_CODE_LENGTH-1 generate
 	--	code(i) <= next_value when i = current_index else unaffected;
 	--end generate code_assigner;
-	
-	process(clock, next_value)
-	begin
-		if (clock'event and clock = '0') then
-			for i in 0 to MAX_CODE_LENGTH-1 loop
-				case current_index is
-					when i => code(i) <= next_value;
-					when others => code(i) <= unaffected;
-				end case;
-			end loop;
-		end if;
-	end process;
 	
 	-- [DEBUG]
 	code_test0 <= code(0);
