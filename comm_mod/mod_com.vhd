edb8a998a4052afee792c56bc34128d6b779000b
mazucanti <daniel.mazucanti.ismart@gmail.com>
2019-08-23 12:00:00 -0300

    modulo de controle

--- comm_mod/mod_com.vhd
+++ comm_mod/mod_com.vhd
@@ -13,10 +13,44 @@ use ieee.std_logic_1164.all;
 use work.code_types.all;
 
 entity mod_com is
-	port (clk, enter : in std_logic; --enter: controle de envio de mensagem
-			code : inout integer_vector); --Mensagem a ser enviadarecebida
+	port --(clk, enter, r_w, recieve : in std_logic; 
+			(clk, r_w, recieve: in std_logic;
+			--enter: controle de envio de mensagem
+			--r_w controla o modo de escrita ou registro de mensagem
+			--recebe sinal de permissao
+			--code : inout integer_vector; 
+			--Mensagem a ser enviada ou recebida
+			r_perm, w_perm, send: out std_logic); 
+			--send envia o sinal de permissao
+			--w_perm gera um led indicador de que algo pode ser recebido
+			--r_perm gera um led indicador de que algo pode ser enviado
 end entity;
 
 --Arquitetura de envio de mensagem
 architecture send of mod_com is
-	
+	begin
+	--r_w e diferente para cada placa, por isso essa etapa extra
+	send <= r_w;
+	process(clk)
+	--reduçao de clock para 4Hz
+		variable count : integer range 0 to 12500000;
+		begin
+			if(recieve = '0') then
+				if count < 12500000 then
+					count := count +1;
+					r_perm <= '0';
+				else
+					r_perm <= '1';
+					count := 0;
+				end if;
+			else
+				if count < 12500000 then
+					count := count +1;
+					w_perm <= '0';
+				else
+					w_perm <= '1';
+					count := 0;
+				end if;
+			end if;
+	end process;
+end architecture;
